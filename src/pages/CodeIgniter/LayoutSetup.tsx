953cc4eb43608306c22ac2a24495e8ddc316d591
Pavan Kumar <project.manager@amareatechsolutions.com>
2025-08-29 17:29:57 +0530

    Removed unnecessary imports

--- src/pages/CodeIgniter/LayoutSetup.tsx
+++ src/pages/CodeIgniter/LayoutSetup.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer"; // Import Footer
+import Footer from "@/components/layout/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Separator } from "@/components/ui/separator";
 
@@ -1018,4 +1017,4 @@ class Home extends Controller
   );
 };
 
-export default LayoutSetup;
+export default LayoutSetup;;;
--- src/pages/Git/GitflowWorkflow.tsx
+++ src/pages/Git/GitflowWorkflow.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
